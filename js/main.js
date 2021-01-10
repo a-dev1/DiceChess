60e870d30ccfa6be24a3e8be26f2edea8952947b
Mohammed Bilal <r.mohammedbilal@gmail.com>
2021-01-10 18:10:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var game;
 var flip_button = document.querySelector(".roll-dice-btn");     
 
 const rollTheDice = () => {
-    fetch('http://b860a8d9d071.eu.ngrok.io/roll')
+    fetch('https://b860a8d9d071.eu.ngrok.io/roll')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log(err))
